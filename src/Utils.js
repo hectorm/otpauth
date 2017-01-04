7660f2f708fd4163f4d83f4c83578b1bfe0291b1
Héctor Molinero Fernández <hector@molinero.xyz>
2017-01-04 16:28:10 +0100

    Refactor

--- src/Utils.js
+++ src/Utils.js
@@ -1,260 +1,196 @@
 'use strict';
 
 /**
- * @class Utils
+ * An object containing some utilities.
+ * @type {Object}
+ *
+ * @property {function(buf: ArrayBuffer): number} uint.decode
+ *   Converts an ArrayBuffer to an integer.
+ * @property {function(num: number): ArrayBuffer} uint.encode
+ *   Converts an integer to an ArrayBuffer.
+ *
+ * @property {function(buf: ArrayBuffer): string} raw.decode
+ *   Converts an ArrayBuffer to a string.
+ * @property {function(str: string): ArrayBuffer} raw.encode
+ *   Converts a string to an ArrayBuffer.
+ *
+ * @property {function(buf: ArrayBuffer): string} b32.decode
+ *   Converts an ArrayBuffer to a base32 string (RFC 4648).
+ * @property {function(str: string): ArrayBuffer} b32.encode
+ *   Converts a base32 string to an ArrayBuffer (RFC 4648).
+ *
+ * @property {function(buf: ArrayBuffer): string} hex.decode
+ *   Converts an ArrayBuffer to a hexadecimal string.
+ * @property {function(str: string): ArrayBuffer} hex.encode
+ *   Converts a hexadecimal string to an ArrayBuffer.
  */
-export class Utils {
-	/**
-	 * @type {Object}
-	 */
-	static get uint() {
-		/**
-		 * Converts an ArrayBuffer to an integer.
-		 * @memberof Utils.uint
-		 * @method decode
-		 * @param {ArrayBuffer} buff ArrayBuffer.
-		 * @returns {number} Integer.
-		 */
-		const decode = function (buff) {
-			const tArr = new Uint8Array(buff);
-			let num = 0;
+export const Utils = {
+	'uint': {}, 'raw': {}, 'b32': {}, 'hex': {}
+};
 
-			for (let i = 0; i < tArr.length; i++) {
-				if (tArr[i] === 0) continue;
-				num *= 256;
-				num += tArr[i];
-			}
+Utils.uint.decode = function (buf) {
+	const arr = new Uint8Array(buf);
+	let num = 0;
 
-			return num;
-		};
-
-		/**
-		 * Converts an integer to an ArrayBuffer.
-		 * @memberof Utils.uint
-		 * @method encode
-		 * @param {string} num Integer.
-		 * @returns {ArrayBuffer} ArrayBuffer.
-		 */
-		const encode = function (num) {
-			const buff = new ArrayBuffer(8);
-			const tArr = new Uint8Array(buff);
-			let acc = num;
-
-			for (let i = 7; i >= 0; i--) {
-				if (acc === 0) break;
-				tArr[i] = acc & 255;
-				acc -= tArr[i];
-				acc /= 256;
-			}
-
-			return buff;
-		};
-
-		return {decode, encode};
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === 0) continue;
+		num *= 256;
+		num += arr[i];
 	}
 
-	/**
-	 * @type {Object}
-	 */
-	static get raw() {
-		/**
-		 * Converts an ArrayBuffer to a string.
-		 * @memberof Utils.raw
-		 * @method decode
-		 * @param {ArrayBuffer} buff ArrayBuffer.
-		 * @returns {string} String.
-		 */
-		const decode = function (buff) {
-			const arr = new Uint8Array(buff);
-			let str = '';
+	return num;
+};
 
-			for (let i = 0; i < arr.length; i++) {
-				str += String.fromCharCode(arr[i]);
-			}
+Utils.uint.encode = function (num) {
+	const buf = new ArrayBuffer(8);
+	const arr = new Uint8Array(buf);
+	let acc = num;
 
-			return str;
-		};
-
-		/**
-		 * Converts a string to an ArrayBuffer.
-		 * @memberof Utils.raw
-		 * @method encode
-		 * @param {string} str String.
-		 * @returns {ArrayBuffer} ArrayBuffer.
-		 */
-		const encode = function (str) {
-			const buff = new ArrayBuffer(str.length);
-			const tArr = new Uint8Array(buff);
-
-			for (let i = 0; i < str.length; i++) {
-				tArr[i] = str.charCodeAt(i);
-			}
-
-			return buff;
-		};
-
-		return {decode, encode};
+	for (let i = 7; i >= 0; i--) {
+		if (acc === 0) break;
+		arr[i] = acc & 255;
+		acc -= arr[i];
+		acc /= 256;
 	}
 
-	/**
-	 * @type {Object}
-	 */
-	static get b32() {
-		// RFC 4648 base32 alphabet without pad
-		const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
+	return buf;
+};
 
-		/**
-		 * Converts an ArrayBuffer to a base32 string (RFC 4648).
-		 * @see https://tools.ietf.org/html/rfc4648
-		 * @see https://github.com/LinusU/base32-decode
-		 * @memberof Utils.b32
-		 * @method decode
-		 * @param {ArrayBuffer} buff ArrayBuffer.
-		 * @returns {string} Base32 string.
-		 */
-		const decode = function (buff) {
-			// Based on LinusU/base32-decode
-			const tArr = new Uint8Array(buff);
-			let bits = 0, value = 0, str = '';
+Utils.raw.decode = function (buf) {
+	const arr = new Uint8Array(buf);
+	let str = '';
 
-			for (let i = 0; i < tArr.length; i++) {
-				value = value << 8 | tArr[i];
-				bits += 8;
-
-				while (bits >= 5) {
-					str += alphabet[value >>> bits - 5 & 31];
-					bits -= 5;
-				}
-			}
-
-			if (bits > 0) {
-				str += alphabet[value << 5 - bits & 31];
-			}
-
-			return str;
-		};
-
-		/**
-		 * Converts a base32 string to an ArrayBuffer (RFC 4648).
-		 * @see https://tools.ietf.org/html/rfc4648
-		 * @see https://github.com/LinusU/base32-encode
-		 * @memberof Utils.b32
-		 * @method encode
-		 * @param {string} str Base32 String.
-		 * @returns {ArrayBuffer} ArrayBuffer.
-		 */
-		const encode = function (str) {
-			// Based on LinusU/base32-encode
-			const strU = str.toUpperCase();
-			const buff = new ArrayBuffer(str.length * 5 / 8 | 0);
-			const tArr = new Uint8Array(buff);
-			let bits = 0, value = 0, index = 0;
-
-			for (let i = 0; i < strU.length; i++) {
-				let idx = alphabet.indexOf(strU[i]);
-
-				if (idx === -1) {
-					throw new Error('Invalid character found: ' + strU[i]);
-				}
-
-				value = value << 5 | idx;
-				bits += 5;
-
-				if (bits >= 8) {
-					tArr[index++] = value >>> bits - 8 & 255;
-					bits -= 8;
-				}
-			}
-
-			return buff;
-		};
-
-		return {decode, encode};
+	for (let i = 0; i < arr.length; i++) {
+		str += String.fromCharCode(arr[i]);
 	}
 
-	/**
-	 * @type {Object}
-	 */
-	static get hex() {
-		/**
-		 * Converts an ArrayBuffer to a hexadecimal string.
-		 * @memberof Utils.hex
-		 * @method decode
-		 * @param {ArrayBuffer} buff ArrayBuffer.
-		 * @returns {string} Hexadecimal string.
-		 */
-		const decode = function (buff) {
-			const tArr = new Uint8Array(buff);
-			let str = '';
+	return str;
+};
 
-			for (let i = 0; i < tArr.length; i++) {
-				const hexByte = tArr[i].toString(16);
+Utils.raw.encode = function (str) {
+	const buf = new ArrayBuffer(str.length);
+	const arr = new Uint8Array(buf);
 
-				str += hexByte.length === 1
-					? '0' + hexByte
-					: hexByte;
-			}
-
-			return str.toUpperCase();
-		};
-
-		/**
-		 * Converts a hexadecimal string to an ArrayBuffer.
-		 * @memberof Utils.hex
-		 * @method encode
-		 * @param {string} str Hexadecimal string.
-		 * @returns {ArrayBuffer} ArrayBuffer.
-		 */
-		const encode = function (str) {
-			const buff = new ArrayBuffer(str.length / 2);
-			const tArr = new Uint8Array(buff);
-
-			for (let i = 0; i < tArr.length; i++) {
-				tArr[i] = parseInt(str.substr(i * 2, 2), 16);
-			}
-
-			return buff;
-		};
-
-		return {decode, encode};
+	for (let i = 0; i < str.length; i++) {
+		arr[i] = str.charCodeAt(i);
 	}
-}
+
+	return buf;
+};
+
+// RFC 4648 base32 alphabet without pad
+Utils.b32.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
+
+// Based on github.com/LinusU/base32-decode
+Utils.b32.decode = function (buf) {
+	const arr = new Uint8Array(buf);
+	let bits = 0, value = 0, str = '';
+
+	for (let i = 0; i < arr.length; i++) {
+		value = value << 8 | arr[i];
+		bits += 8;
+
+		while (bits >= 5) {
+			str += Utils.b32.alphabet[value >>> bits - 5 & 31];
+			bits -= 5;
+		}
+	}
+
+	if (bits > 0) {
+		str += Utils.b32.alphabet[value << 5 - bits & 31];
+	}
+
+	return str;
+};
+
+// Based on github.com/LinusU/base32-encode
+Utils.b32.encode = function (str) {
+	const strUpp = str.toUpperCase();
+	const buf = new ArrayBuffer(str.length * 5 / 8 | 0);
+	const arr = new Uint8Array(buf);
+	let bits = 0, value = 0, index = 0;
+
+	for (let i = 0; i < strUpp.length; i++) {
+		let idx = Utils.b32.alphabet.indexOf(strUpp[i]);
+
+		if (idx === -1) {
+			throw new Error('Invalid character found: ' + strUpp[i]);
+		}
+
+		value = value << 5 | idx;
+		bits += 5;
+
+		if (bits >= 8) {
+			arr[index++] = value >>> bits - 8 & 255;
+			bits -= 8;
+		}
+	}
+
+	return buf;
+};
+
+Utils.hex.decode = function (buf) {
+	const arr = new Uint8Array(buf);
+	let str = '';
+
+	for (let i = 0; i < arr.length; i++) {
+		const hexByte = arr[i].toString(16);
+
+		str += hexByte.length === 1
+			? '0' + hexByte
+			: hexByte;
+	}
+
+	return str.toUpperCase();
+};
+
+Utils.hex.encode = function (str) {
+	const buf = new ArrayBuffer(str.length / 2);
+	const arr = new Uint8Array(buf);
+
+	for (let i = 0; i < arr.length; i++) {
+		arr[i] = parseInt(str.substr(i * 2, 2), 16);
+	}
+
+	return buf;
+};
 
 /**
- * @class _Utils
+ * An object containing some utilities (private).
+ * @type {Object}
  * @private
  */
-export class _Utils {
-	static buf2arrbuf(buf) {
-		if (buf.buffer instanceof ArrayBuffer) {
-			return buf.buffer;
-		}
+export const _Utils = {};
 
-		// Node.js < 4.0.0
-		const arrbuf = new ArrayBuffer(buf.length);
-		const arr = new Uint8Array(arrbuf);
-
-		for (let i = 0; i < arr.length; i++) {
-			arr[i] = buf[i];
-		}
-
-		return arrbuf;
+_Utils.buf2arrbuf = function (buf) {
+	if (buf.buffer instanceof ArrayBuffer) {
+		return buf.buffer;
 	}
 
-	static arrbuf2buf(arrbuf) {
-		if (typeof Buffer.from === 'function') {
-			return Buffer.from(arrbuf);
-		}
+	// Node.js < 4.0.0
+	const arrbuf = new ArrayBuffer(buf.length);
+	const arr = new Uint8Array(arrbuf);
 
-		// Node.js < 5.10.0
-		const buf = new Buffer(arrbuf.byteLength);
-		const arr = new Uint8Array(arrbuf);
-
-		for (let i = 0; i < arr.length; i++) {
-			buf[i] = arr[i];
-		}
-
-		return buf;
+	for (let i = 0; i < arr.length; i++) {
+		arr[i] = buf[i];
 	}
-}
+
+	return arrbuf;
+};
+
+_Utils.arrbuf2buf = function (arrbuf) {
+	if (typeof Buffer.from === 'function') {
+		return Buffer.from(arrbuf);
+	}
+
+	// Node.js < 5.10.0
+	const buf = new Buffer(arrbuf.byteLength);
+	const arr = new Uint8Array(arrbuf);
+
+	for (let i = 0; i < arr.length; i++) {
+		buf[i] = arr[i];
+	}
+
+	return buf;
+};
 
