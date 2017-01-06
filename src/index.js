0e9cd739ea4a135af7ce665dadc707b4665dc058
Héctor Molinero Fernández <hector@molinero.xyz>
2017-01-06 03:38:56 +0100

    Use default export in "index.js"

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 'use strict';
 
-export {Utils} from './Utils.js';
-export {Secret} from './Secret.js';
-export {HMAC} from './HMAC.js';
-export {HOTP, TOTP} from './OTP.js';
-export {URI} from './URI.js';
+import {HOTP, TOTP} from './OTP.js';
+import {HMAC} from './HMAC.js';
+import {URI} from './URI.js';
+import {Secret} from './Secret.js';
+import {Utils} from './Utils.js';
+
+/**
+ * One Time Password (HOTP/TOTP) library for Node.js and browser.
+ * @author Héctor Molinero Fernández <hector@molinero.xyz>
+ * @type {Object}
+ */
+const OTPAuth = {
+	'HOTP': HOTP,
+	'TOTP': TOTP,
+	'HMAC': HMAC,
+	'URI': URI,
+	'Secret': Secret,
+	'Utils': Utils
+};
+
+export default OTPAuth;
+module.exports = OTPAuth;
 
