0ba9272287154e241df463c808dc09907cf5fac7
Héctor Molinero <hector@molinero.xyz>
2017-07-22 16:53:13 +0200

    Export SJCL as ES6 module

--- webpack.config.js
+++ webpack.config.js
@@ -18,31 +18,29 @@ module.exports = {
 		path: path.join(__dirname, 'dist')
 	},
 	devtool: 'source-map',
-	node: {
-		Buffer: false
-	},
+	node: {Buffer: false},
 	plugins: [
 		// Custom build of the Stanford Javascript Crypto Library (SJCL)
 		new webpack.NormalModuleReplacementPlugin(/^sjcl$/, function (result) {
-			const request = tmp.fileSync().name;
+			const file = tmp.fileSync();
 
-			const root = path.join(__dirname, 'node_modules/sjcl');
-			const sources = [
+			const base = path.join(__dirname, 'node_modules/sjcl');
+			const fragments = [
 				'core/sjcl.js',
 				'core/bitArray.js',
 				'core/codecArrayBuffer.js',
 				'core/sha1.js',
 				'core/sha256.js',
 				'core/sha512.js',
-				'core/hmac.js',
-				'core/exports.js'
+				'core/hmac.js'
 			];
 
-			fs.writeFileSync(request, sources.reduce(function (code, file) {
-				return code + fs.readFileSync(path.join(root, file));
-			}, ''));
+			let code = fragments.reduce(function (content, fragment) {
+				return content + fs.readFileSync(path.join(base, fragment));
+			}, '') + '; export default sjcl;';
 
-			result.request = request;
+			fs.writeFileSync(file.name, code);
+			result.request = file.name;
 		}),
 		new ClosureCompilerPlugin({
 			options: {
@@ -50,14 +48,15 @@ module.exports = {
 				languageIn: 'ES6_STRICT',
 				languageOut: 'ES5_STRICT',
 				compilationLevel: 'SIMPLE',
-				createSourceMap: true,
 				rewritePolyfills: true,
+				createSourceMap: true,
 				assumeFunctionWrapper: true,
 				outputWrapper: '(function(){%output%}).call(this);',
 				externs: [
-					{src: 'var define'},
-					{src: 'var exports'},
-					{src: 'var module'}
+					{src: 'let Buffer'},
+					{src: 'let define'},
+					{src: 'let exports'},
+					{src: 'let module'}
 				]
 			}
 		}),
