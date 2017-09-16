13e30952db0937bacc28a3165016ab59753b2442
Héctor Molinero Fernández <hector@molinero.xyz>
2017-09-16 10:32:45 +0200

    Add missing JSDoc annotation

--- src/index.js
+++ src/index.js
@@ -7,4 +7,9 @@ export {HOTP, TOTP} from './otp.js';
 export {URI} from './uri.js';
 export {Secret} from './secret.js';
 export {Utils} from './utils.js';
+
+/**
+ * Library version.
+ * @type {string}
+ */
 export const version = process.env.VERSION;
