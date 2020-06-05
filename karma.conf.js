bb098e517d18136466fc2bb19a33ac842f142007
Héctor Molinero Fernández <hector@molinero.dev>
2020-06-05 18:13:40 +0200

    Removed karma-chai

--- karma.conf.js
+++ karma.conf.js
@@ -1,21 +1,19 @@
 
-const path = require('path');
-
 module.exports = config => {
 	config.set({
-		frameworks: ['mocha', 'chai'],
+		frameworks: ['mocha'],
 		files: [
-			path.join(__dirname, process.env.IS_MINIFIED
-				? 'dist/otpauth.umd.min.js'
-				: 'dist/otpauth.umd.js'),
-			path.join(__dirname, 'test/test.js')
+			require.resolve('chai/chai.js'),
+			process.env.IS_MINIFIED
+				? './dist/otpauth.umd.min.js'
+				: './dist/otpauth.umd.js',
+			'./test/test.js'
 		],
 		reporters: ['dots'],
 		singleRun: true,
 		autoWatch: false,
 		browsers: ['FirefoxHeadless', 'ChromeHeadless'],
 		plugins: [
-			'karma-chai',
 			'karma-chrome-launcher',
 			'karma-firefox-launcher',
 			'karma-mocha'
