16e945782a03220a46726a67c818a0a0152ace80
Héctor Molinero Fernández <hector@molinero.dev>
2021-01-17 12:50:11 +0100

    6.2.1

--- dist/otpauth.cjs.js
+++ dist/otpauth.cjs.js
@@ -1,5 +1,5 @@
 
-/*! otpauth v6.2.0 | (c) Héctor Molinero Fernández <hector@molinero.dev> | MIT | https://github.com/hectorm/otpauth */
+/*! otpauth v6.2.1 | (c) Héctor Molinero Fernández <hector@molinero.dev> | MIT | https://github.com/hectorm/otpauth */
 /*! sjcl v1.0.8 | (c) bitwiseshiftleft | (BSD-2-Clause OR GPL-2.0-only) | https://github.com/bitwiseshiftleft/sjcl */
 'use strict';
 
@@ -2680,7 +2680,7 @@ var URI = /*#__PURE__*/function () {
  * Library version.
  * @type {string}
  */
-var version = '6.2.0';
+var version = '6.2.1';
 
 exports.HOTP = HOTP;
 exports.Secret = Secret;
