b36b434b8cfaa44168bef9b32cc230feffb2c45a
Héctor Molinero Fernández <hector@molinero.dev>
2021-04-11 15:26:59 +0200

    Generate license banner dynamically

--- rollup.config.js
+++ rollup.config.js
@@ -3,45 +3,60 @@ import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
-import otpauthPkg from './package.json';
+import packageJson from './package.json';
 
-const minBuildOptions = {
-	plugins: [
-		/* eslint-disable-next-line camelcase */
-		terser({ output: { max_line_len: 1024 } })
-	],
-	sourcemap: true
-};
+export default async () => {
+	const commonBuildOptions = {
+		name: 'OTPAuth',
+		banner: async () => {
+			const getLicenseComment = pkg => (`/*! ${[
+				`${pkg.name} v${pkg.version}`,
+				`(c) ${pkg.author.name ? pkg.author.name : pkg.author}`,
+				`${pkg.license}`,
+				`${pkg.homepage}`
+			].join(' | ')} */\n`);
 
-const getBanner = pkg => (`/*! ${[
-	`${pkg.name} v${pkg.version}`,
-	`(c) ${pkg.author.name ? pkg.author.name : pkg.author}`,
-	`${pkg.license}`,
-	`${pkg.homepage}`
-].join(' | ')} */`);
+			let comment = getLicenseComment(packageJson);
+			for (const dependency of Object.keys(packageJson.dependencies)) {
+				// eslint-disable-next-line no-await-in-loop
+				const dependencyPackageJson = await import(`${dependency}/package.json`);
+				comment += getLicenseComment(dependencyPackageJson);
+			}
 
-export default {
-	input: 'src/main.js',
-	output: [
-		{ name: 'OTPAuth', file: './dist/otpauth.cjs.js', format: 'cjs' },
-		{ name: 'OTPAuth', file: './dist/otpauth.umd.js', format: 'umd' },
-		{ name: 'OTPAuth', file: './dist/otpauth.esm.js', format: 'es' },
-		{ name: 'OTPAuth', file: './dist/otpauth.cjs.min.js', format: 'cjs', ...minBuildOptions },
-		{ name: 'OTPAuth', file: './dist/otpauth.umd.min.js', format: 'umd', ...minBuildOptions },
-		{ name: 'OTPAuth', file: './dist/otpauth.esm.min.js', format: 'es', ...minBuildOptions }
-	],
-	plugins: [
-		replace({ preventAssignment: true, __OTPAUTH_VERSION__: otpauthPkg.version }),
-		resolve(),
-		babel({ babelHelpers: 'bundled' }),
-		{ banner: [getBanner(otpauthPkg)].join('\n') }
-	],
-	onwarn: warning => {
-		// Ignore "eval" in "utils.js".
-		if (warning.code === 'EVAL'
-			&& /\/utils\.js$/.test(warning.loc.file)
-		) return;
+			return comment;
+		}
+	};
 
-		throw new Error(warning.message);
-	}
+	const minifiedBuildOptions = {
+		plugins: [
+			/* eslint-disable-next-line camelcase */
+			terser({ output: { max_line_len: 1024 } })
+		],
+		sourcemap: true
+	};
+
+	return {
+		input: './src/main.js',
+		output: [
+			{ file: './dist/otpauth.cjs.js', format: 'cjs', ...commonBuildOptions },
+			{ file: './dist/otpauth.umd.js', format: 'umd', ...commonBuildOptions },
+			{ file: './dist/otpauth.esm.js', format: 'es', ...commonBuildOptions },
+			{ file: './dist/otpauth.cjs.min.js', format: 'cjs', ...commonBuildOptions, ...minifiedBuildOptions },
+			{ file: './dist/otpauth.umd.min.js', format: 'umd', ...commonBuildOptions, ...minifiedBuildOptions },
+			{ file: './dist/otpauth.esm.min.js', format: 'es', ...commonBuildOptions, ...minifiedBuildOptions }
+		],
+		plugins: [
+			replace({ preventAssignment: true, __OTPAUTH_VERSION__: packageJson.version }),
+			resolve(),
+			babel({ babelHelpers: 'bundled' })
+		],
+		onwarn: warning => {
+			// Ignore "eval" in "utils.js".
+			if (warning.code === 'EVAL' && /\/utils\.js$/.test(warning.loc.file)) {
+				return;
+			}
+
+			throw new Error(warning.message);
+		}
+	};
 };
