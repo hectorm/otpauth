31fff003d7904b28a3cae4c21d66d3ed719fb50d
Héctor Molinero Fernández <hector@molinero.dev>
2022-02-23 19:42:46 +0100

    7.0.11

--- dist/otpauth.umd.js
+++ dist/otpauth.umd.js
@@ -1,4 +1,4 @@
-/*! otpauth v7.0.10 | (c) Héctor Molinero Fernández <hector@molinero.dev> | MIT | https://github.com/hectorm/otpauth */
+/*! otpauth v7.0.11 | (c) Héctor Molinero Fernández <hector@molinero.dev> | MIT | https://github.com/hectorm/otpauth */
 /*! jssha v3.2.0 | (c) Brian Turek <brian.turek@gmail.com> | BSD-3-Clause | https://github.com/Caligatio/jsSHA */
 
 (function (global, factory) {
@@ -2299,7 +2299,7 @@
    * Library version.
    * @type {string}
    */
-  var version = "7.0.10";
+  var version = "7.0.11";
 
   exports.HOTP = HOTP;
   exports.Secret = Secret;
