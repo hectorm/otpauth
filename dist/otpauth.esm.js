ba0252d75441ce1d01e9326258519500962ae4f2
Héctor Molinero Fernández <hector@molinero.dev>
2022-04-02 13:11:03 +0200

    7.1.1

--- dist/otpauth.esm.js
+++ dist/otpauth.esm.js
@@ -1,4 +1,4 @@
-/*! otpauth v7.1.0 | (c) Héctor Molinero Fernández <hector@molinero.dev> | MIT | https://github.com/hectorm/otpauth */
+/*! otpauth v7.1.1 | (c) Héctor Molinero Fernández <hector@molinero.dev> | MIT | https://github.com/hectorm/otpauth */
 /*! jssha v3.2.0 | (c) Brian Turek <brian.turek@gmail.com> | BSD-3-Clause | https://github.com/Caligatio/jsSHA */
 
 function _typeof(obj) {
@@ -2286,6 +2286,6 @@ var URI = /*#__PURE__*/function () {
  * Library version.
  * @type {string}
  */
-var version = "7.1.0";
+var version = "7.1.1";
 
 export { HOTP, Secret, TOTP, URI, version };
