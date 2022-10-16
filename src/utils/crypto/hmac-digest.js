279147f978f4fd95af07571da3a79a31d5f181e1
Héctor Molinero Fernández <hector@molinero.dev>
2022-10-16 12:55:35 +0200

    Add type definition for jsSHA algorithms

--- src/utils/crypto/hmac-digest.js
+++ src/utils/crypto/hmac-digest.js
@@ -5,7 +5,7 @@ import { globalScope } from "../global-scope.js";
 
 /**
  * OpenSSL to jsSHA algorithms map.
- * @type {Object.<string, string>}
+ * @type {Object.<string, "SHA-1"|"SHA-224"|"SHA-256"|"SHA-384"|"SHA-512"|"SHA3-224"|"SHA3-256"|"SHA3-384"|"SHA3-512">}
  */
 const OPENSSL_JSSHA_ALGO_MAP = {
   SHA1: "SHA-1",
@@ -37,7 +37,6 @@ const hmacDigest = (algorithm, key, message) => {
     if (typeof variant === "undefined") {
       throw new TypeError("Unknown hash function");
     }
-    // @ts-ignore
     const hmac = new jsSHA(variant, "ARRAYBUFFER");
     hmac.setHMACKey(key, "ARRAYBUFFER");
     hmac.update(message);
